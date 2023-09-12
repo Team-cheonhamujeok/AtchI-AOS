95c1117518f64ea537aba883a8f946a24ef0a421
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-09-13 00:59:27 +0900

    feat: TextField Components 추가

--- app/src/main/java/com/example/atchi_aos/ui/components/Field.kt
+++ app/src/main/java/com/example/atchi_aos/ui/components/Field.kt
@@ -1,9 +1,69 @@
 package com.example.atchi_aos.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
+fun PurpleLightFiled(
+    text: String
+) {
+    val purpleLight = Modifier
+        .border(
+            width = 2.dp,
+            color = Color(0x337544C6),
+            shape = RoundedCornerShape(size = 20.dp)
+        )
+        .width(340.dp)
+        .height(65.dp)
+        .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(size = 20.dp))
+        .padding(start = 28.dp, top = 23.dp, end = 86.dp, bottom = 23.dp)
 
-fun AtchIField() {
+    Box(modifier = purpleLight) {
+        Text(text = "$text")
+    }
+}
+
+@Composable
+fun PurpleFiled(
+    text: String
+) {
+    val purpleLight = Modifier
+        .border(
+            width = 2.dp,
+            color = Color(0xFF7544C6),
+            shape = RoundedCornerShape(size = 20.dp)
+        )
+        .width(340.dp)
+        .height(65.dp)
+        .background(color = Color(0xFFFFFFFF), shape = RoundedCornerShape(size = 20.dp))
+        .padding(start = 28.dp, top = 23.dp, end = 86.dp, bottom = 23.dp)
+
+    Box(modifier = purpleLight) {
+        Text(text = "$text")
+    }
+}
+
+@Preview
+@Composable
+fun PurpleLightFiledPreview() {
+    PurpleLightFiled(text = "hello world")
 
 }
+
+@Preview
+@Composable
+fun PurpleFieldPreview() {
+    PurpleFiled(text = "hello world")
+}
+
