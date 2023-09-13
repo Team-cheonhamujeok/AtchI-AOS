8180d536ae4a69c5d15ccd2f89ae836386fa173f
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-09-13 22:46:14 +0900

    edit: Button align edit

--- app/src/main/java/com/example/atchi_aos/ui/components/Button.kt
+++ app/src/main/java/com/example/atchi_aos/ui/components/Button.kt
@@ -1,14 +1,17 @@
 package com.example.atchi_aos.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
@@ -22,28 +25,35 @@ import androidx.compose.ui.unit.sp
 fun PurpleButton(
     text: String
 ) {
-    Button(
-        colors = ButtonDefaults
-            .buttonColors(containerColor = Color(0xFF7544C6), contentColor = Color.White),
-        onClick = { /*TODO*/ }
-    ) {
-        Text(
-            text = text,
-            modifier = Modifier
-            .width(340.dp)
-            .height(65.dp)
-            .background(color = Color(0xFF7544C6), shape = RoundedCornerShape(size = 20.dp))
-            .padding(start = 44.dp, top = 18.dp, end = 44.dp, bottom = 18.dp),
-            // Title/Small
-            style = TextStyle(
-                fontSize = 20.sp,
-                lineHeight = 28.sp,
-                //fontFamily = FontFamily(Font(R.font.apple sd gothic neo)),
-                fontWeight = FontWeight(600),
-                color = Color(0xFFFFFFFF),
-                textAlign = TextAlign.Center,
+    val modifier = Modifier
+        .width(340.dp)
+        .height(65.dp)
+        .background(color = Color(0xFF7544C6), shape = RoundedCornerShape(size = 20.dp))
+
+    Column(modifier = modifier) {
+        Button(
+            modifier = Modifier.fillMaxSize(),
+            colors = ButtonDefaults
+                .buttonColors(containerColor = Color(0xFF7544C6), contentColor = Color.White),
+            onClick = { /*TODO*/ }
+        ) {
+            Text(
+                text = text,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .wrapContentHeight(align = Alignment.CenterVertically)
+                    .background(color = Color(0xFF7544C6)),
+                // Title/Small
+                style = TextStyle(
+                    fontSize = 20.sp,
+                    lineHeight = 28.sp,
+                    //fontFamily = FontFamily(Font(R.font.apple sd gothic neo)),
+                    fontWeight = FontWeight(600),
+                    color = Color(0xFFFFFFFF),
+                    textAlign = TextAlign.Center
+                )
             )
-        )
+        }
     }
 }
 
