bb426b6455d84187c946e0e8d8a0685024335fff
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-09-15 22:09:26 +0900

    feat: Card Component 초기화

---
+++ app/src/main/java/com/example/atchi_aos/ui/components/Card.kt
@@ -0,0 +1,17 @@
+package com.example.atchi_aos.ui.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Card(
+
+) {
+
+}
+
+@Preview
+@Composable
+fun CardPreview() {
+    Card()
+}
