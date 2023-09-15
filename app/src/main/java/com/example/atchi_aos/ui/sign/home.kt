21c143ff18774461882141ade6fa7cc0726174c4
Mingyu Kang <57595198+KoreaMango@users.noreply.github.com>
2023-09-15 22:06:41 +0900

    Add: Base View Template 생성

---
+++ app/src/main/java/com/example/atchi_aos/ui/sign/home.kt
@@ -0,0 +1,29 @@
+package com.example.atchi_aos.ui.sign
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun Home() {
+
+}
+
+@Composable
+private fun Header() {
+
+}
+@Composable
+private fun Body() {
+
+}
+
+@Composable
+private fun Bottom() {
+
+}
+
+@Preview
+@Composable
+fun HomePreview() {
+    Home()
+}
